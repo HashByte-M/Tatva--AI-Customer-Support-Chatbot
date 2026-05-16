b7feaa47151abbe0ebbfda414756b682f851ff1a
Prateek Modanwal <prateekmodanwal.work@gmail.com>
2026-05-17 03:17:30 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -87,7 +87,7 @@ app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["tatva-chatbot.netlify.app","https://adishila.in/","adishila.in"], 
+    allow_origins=["https://tatva-chatbot.netlify.app","https://adishila.in","adishila.in"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
